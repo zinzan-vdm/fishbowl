370250d4d6a5f16fc9b8f9d7ac8410cb611046fa
zinzan <inq2111@gmail.com>
2024-10-22 13:38:11 +0200

    dots [2024-10-22T13:38:11+02:00] [config-dots-push]

--- .dots/default/.bashrc.d/functions/atuin.sh
+++ .dots/default/.bashrc.d/functions/atuin.sh
@@ -2,7 +2,9 @@
 
 if command -v atuin &> /dev/null; then
   atuin init bash > /tmp/.atuin-init
-  source $HOME/.bashrc.resources.d/functions/bash-preexec/bash-preexec.sh
+
   source /tmp/.atuin-init
+
+  source $HOME/.bashrc.resources.d/functions/bash-preexec/bash-preexec.sh
 fi
 
