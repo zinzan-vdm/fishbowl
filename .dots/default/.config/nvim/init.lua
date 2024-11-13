e9674cb7a1f3d2d44b1edfc82b705b6c4166d8e2
zinzan <inq2111@gmail.com>
2024-11-13 12:54:30 +0100

    dots [2024-11-13T12:54:30+01:00] [config-dots-push]

--- .dots/default/.config/nvim/init.lua
+++ .dots/default/.config/nvim/init.lua
@@ -706,7 +706,48 @@ require('lazy').setup({
 				auto_apply_diff_after_generation = false,
 				support_paste_from_clipboard = false,
 			},
-			mappings = {}, -- default mappings
+			mappings = {
+				ask = "<leader>aa",
+				edit = "<leader>ae",
+				refresh = "<leader>ar",
+				focus = "<leader>af",
+				diff = {
+					ours = "co",
+					theirs = "ct",
+					all_theirs = "ca",
+					both = "cb",
+					cursor = "cc",
+					next = "]x",
+					prev = "[x",
+				},
+				suggestion = {
+					accept = "<leader>asa",
+					next = "<leader>asn",
+					prev = "<leader>asN",
+					dismiss = "<leader>asd",
+				},
+				jump = {
+					next = "]]",
+					prev = "[[",
+				},
+				submit = {
+					normal = "<CR>",
+					insert = "<C-s>",
+				},
+				toggle = {
+					default = "<leader>at",
+					debug = "<leader>ad",
+					hint = "<leader>ah",
+					suggestion = "<leader>as",
+					repomap = "<leader>aR",
+				},
+				sidebar = {
+					apply_all = "A",
+					apply_cursor = "a",
+					switch_windows = "<Tab>",
+					reverse_switch_windows = "<S-Tab>",
+				},
+			},
 			hints = { enabled = false }, -- hints on lines, it sucks
 			windows = {
 				position = "right",
