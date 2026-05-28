027dbc0148d5002e4b64565290fe1a07b93d641f
zinzan <inq2111@gmail.com>
2026-05-28 13:31:20 +0200

    dots [2026-05-28T13:31:20+02:00] [config-dots-push]

--- .dots/headed/.bashrc.d/functions/notifs.sh
+++ .dots/headed/.bashrc.d/functions/notifs.sh
@@ -1,1 +1,15 @@
-alias notifs='dunstctl set-paused toggle'
+# alias notifs='dunstctl set-paused toggle'
+
+function notifs() {
+    case "$1" in
+        on)  dunstctl set-paused false ;;
+        off) dunstctl set-paused true ;;
+        *)   dunstctl set-paused toggle ;;
+    esac
+
+    if dunstctl is-paused | grep -q true; then
+        echo "Notifications: OFF"
+    else
+        echo "Notifications: ON"
+    fi
+}
